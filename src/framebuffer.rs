d70eb19c97c226937cb6efeb5f0c808ccd0f31f2
DijanU <pad23663@uvg.edu.gt>
2025-08-18 16:33:35 -0600

    fell image

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -26,7 +26,8 @@ impl Framebuffer {
 
     pub fn set_pixel(&mut self, x: i32, y: i32) {
         if x >= 0 && x < self.width && y >= 0 && y < self.height {
-            self.color_buffer.draw_pixel(x, y, self.current_color);
+        let flipped_y = self.height - 1 - y;
+        self.color_buffer.draw_pixel(x, flipped_y, self.current_color);
         }
     }
 

83f9523da2c7f3256047dd2e5d9ee705826c88b0
DijanU <pad23663@uvg.edu.gt>
2025-09-02 15:40:09 -0600

    polygon 2

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,7 @@ fn main() {
     let mut framebuffer = Framebuffer::new(800, 600, Color::BLACK);
     framebuffer.set_current_color(Color::RED);
 
-    let poligono1 = [(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383),];
-
+   
 let poligono2 = [
     (321, 335),
     (288, 286),
@@ -23,55 +22,11 @@ let poligono2 = [
     (374, 302),
 ];
 
-let poligono3 = [
-    (377, 249),
-    (411, 197),
-    (436, 249),
-];
 
-let poligono4 = [
-    (413, 177),
-    (448, 159),
-    (502, 88),
-    (553, 53),
-    (535, 36),
-    (676, 37),
-    (660, 52),
-    (750, 145),
-    (761, 179),
-    (672, 192),
-    (659, 214),
-    (615, 214),
-    (632, 230),
-    (580, 230),
-    (597, 215),
-    (552, 214),
-    (517, 144),
-    (466, 180),
-];
-
-let poligono5 = [
-    (682, 175),
-    (708, 120),
-    (735, 148),
-    (739, 170),
-];
-
-
-framebuffer.set_current_color(Color::RED);
-fill_polygon(&mut framebuffer, &poligono1);
 
 framebuffer.set_current_color(Color::BLUE);
 fill_polygon(&mut framebuffer, &poligono2);
 
-framebuffer.set_current_color(Color::GREEN);
-fill_polygon(&mut framebuffer, &poligono3);
-
-framebuffer.set_current_color(Color::YELLOW);
-fill_polygon(&mut framebuffer, &poligono4);
-
-framebuffer.set_current_color(Color::PURPLE);
-fill_polygon(&mut framebuffer, &poligono5);
 
 
     while !rl.window_should_close() {
